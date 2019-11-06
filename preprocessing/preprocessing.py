654a9c2ae9e44d4a732c533fc17e2533efe69223
Piotr Michalak <42346616+Piotr296@users.noreply.github.com>
2019-11-06 18:47:05 +0100

    Changes

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -3,6 +3,9 @@
 Created on Mon Nov  4 15:04:58 2019
 
 @author: Petronium
+
+Simple script to group data by the id and find the average wind speed 
+during the whole year 
 """
 
 import pandas as pd
@@ -10,6 +13,6 @@ import pandas as pd
 fp = 'data.csv'
 
 df = pd.read_csv(fp)
-df_new = pd.DataFrame(df.groupby('Unit of data: ID').mean())
+df_new = pd.DataFrame(df.groupby('Unit of data: ID').mean()).drop(columns='Geometry (point, surface, grid)').dropna()
 
 df_new.to_excel('new_data.xlsx')
