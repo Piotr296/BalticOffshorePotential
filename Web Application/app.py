055c4c61ba4b24518557ae001560c2c53e737c7a
Ange-lina <55083649+Ange-lina@users.noreply.github.com>
2019-12-05 21:56:31 +0100

    Update app.py

--- Web Application/app.py
+++ Web Application/app.py
@@ -18,17 +18,18 @@ def worker():
     conn = psycopg2.connect("dbname='Baltic_project' \
                              host='localhost' \
                              user='postgres' \
-                             password='1Michalak123'")
+                             password='postgres'")
 
     cur = conn.cursor()
     cur.execute("""UPDATE grid_data
-                   SET fuzzyvalue = (bathmean * {}) + (shipmean * {}) + (windmean * {});""".format(wB, wS, wW))
+                   SET fuzzyvalue = (bathmean * {}) + (shipmean * {}) + (windmean * {})
+                   WHERE (pareasmean = 0 AND bufformean = 0);""".format(wB, wS, wW))
 
     conn.commit()
 
     cur.execute("""SELECT id, fuzzyvalue, ST_AsGeoJSON(geom)
                    FROM grid_data
-                   LIMIT 5;""")
+                   ;""")
 
     rows = cur.fetchall()
 
@@ -53,8 +54,20 @@ def worker():
     cur.close()
     conn.close()
 
-    # print(output)
+    #print(output)
+
+    #create an empty geojson file and overwrite it with the output
+    f = open(r"C:\Users\apatsi19\Desktop\PROJECT\Web Application\static\output.geojson", "w+")
+    f.write(output)
+    f.close()
+
+
     return jsonify(output)
+#    """Response(response=output, status=200, mimetype="application/json")"""
+
+
+TEMPLATES_AUTO_RELOAD = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 if __name__ == "__main__":
     app.run(debug=True)
