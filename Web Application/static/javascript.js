390d2c7ee0903ff05f97d4d3c050c0138b2931fa
olga-carmen <55025556+olga-carmen@users.noreply.github.com>
2019-12-09 21:39:27 +0100

    edit eez

--- Web Application/static/javascript.js
+++ Web Application/static/javascript.js
@@ -57,49 +57,7 @@ var classification_LCoE = function (feature, resolution){
     })
   })
 };
-var classification_eez = function (feature, resolution){
-  const territory1 = feature.get('territory1')
-  var layercolor
-  if (territory1 === "Germany") {
-  layercolor='rgb(0, 255, 191, 0.5)';
-  }
-  else if (territory1 === "Russia" ) {
-  layercolor='rgb(0, 255, 0, 0.5)';
-  }
-  else if (territory1 === "Sweden") {
-  layercolor='	rgb(0, 191, 255, 0.5)';
-  }
-  else if (territory1 === "Latvia") {
-  layercolor='rgb(0, 128, 255, 0.5)';
-  }
-  else if (territory1 === "Estonia") {
-  layercolor='rgb(0, 64, 255, 0.5)';
-  }
-  else if (territory1 === "Poland") {
-  layercolor='rgb	rgb(0, 0, 255, 0.5)';
-  }
-  else if (territory1 === "Finland") {
-  layercolor='rgb(64, 0, 255, 0.5)';
-  }
-  else if (territory1 === "Denmark") {
-  layercolor='rgb(128, 0, 255, 0.5)';
-  }
-  else if (territory1 === "Lithuania") {
-  layercolor='rgb(191, 0, 255, 0.5)';
-  }
-  else {
-  layercolor='rgb(0, 50, 0, 0.5)';
-  }
-  return new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: 'rgba(0, 0, 0, 0)',
-      width: 0.5
-    }),
-    fill: new ol.style.Fill({
-      color: layercolor
-    })
-  })
-};
+
 var classification_eez = function (feature, resolution){
   const territory1 = feature.get('territory1')
   var layercolor
