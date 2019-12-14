a0907391c2f585234ffd1bebfe85e08ba40514d9
Ange-lina <55083649+Ange-lina@users.noreply.github.com>
2019-12-14 14:01:04 +0100

    Update statistics.js

--- Web Application/static/javascript/statistics.js
+++ Web Application/static/javascript/statistics.js
@@ -1,9 +1,12 @@
 var circle = new ol.style.Style({
   image: new ol.style.Circle({
     radius: 7,
-    fill: new ol.style.Fill({color: 'green'}),
+    fill: new ol.style.Fill({
+      color: 'rgb(255, 216, 0)'
+    }),
     stroke: new ol.style.Stroke({
-      color: 'black', width: 1
+      color: 'black',
+      width: 1
     })
   })
 });
@@ -95,4 +98,153 @@ map.on('pointermove', function(e) {
   });
 
   e.map.getTargetElement().style.cursor = hit ? 'pointer' : '';
+});
+
+// INTERACTIVE GRAPH
+
+let chartConfig = {
+  type: 'hbullet',
+  title: {
+    text: 'Installed wind power capacity in the Baltic Sea ( May 2018 )',
+    fontColor: '#212121'
+  },
+  plot: {
+    tooltip: {
+      text: '%t: %v MW', //printed text in the tooltip
+      borderRadius: '3px',
+      borderWidth: '0px',
+      fontSize: '12px'
+    },
+    barSpace: '4px',
+    goal: {
+      alpha: 0.8, //opacity
+      borderWidth: '0px'
+    }
+  },
+  plotarea: {
+    margin: '100 20 60 120' //top-right-bottom-left margins
+  },
+  scaleX: {
+    guide: {
+      lineStyle: 'solid',
+      lineWidth: '2px',
+      visible: true
+    },
+    item: {
+      visible: false
+    },
+    label: {
+      text: 'Countries',
+      offsetX: '-70px'
+    }
+  },
+  scaleY: {
+    guide: {
+      visible: false
+    },
+    label: {
+      text: 'Rated Power (maximum capacity) in MW'
+    }
+  },
+  labels: [{
+      text: 'Denmark',
+      backgroundImage: '',
+      hook: 'scale:name=scale-x,index=8',
+      offsetX: '-50px',
+      width: '50px',
+      height: '50px'
+    },
+    {
+      text: 'Estonia',
+      backgroundImage: '',
+      hook: 'scale:name=scale-x,index=7',
+      offsetX: '-50px',
+      width: '50px',
+      height: '50px'
+    },
+    {
+      text: 'Finland',
+      backgroundImage: '',
+      hook: 'scale:name=scale-x,index=6',
+      offsetX: '-50px',
+      width: '50px',
+      height: '50px'
+    },
+    {
+      text: 'Germany',
+      backgroundImage: '',
+      hook: 'scale:name=scale-x,index=5',
+      offsetX: '-50px',
+      width: '50px',
+      height: '50px'
+    },
+    {
+      text: 'Latvia',
+      backgroundImage: '',
+      hook: 'scale:name=scale-x,index=4',
+      offsetX: '-50px',
+      width: '50px',
+      height: '50px'
+    },
+    {
+      text: 'Lithuania',
+      backgroundImage: '',
+      hook: 'scale:name=scale-x,index=3',
+      offsetX: '-50px',
+      width: '50px',
+      height: '50px'
+    },
+    {
+      text: 'Poland',
+      backgroundImage: '',
+      hook: 'scale:name=scale-x,index=2',
+      offsetX: '-50px',
+      width: '50px',
+      height: '50px'
+    },
+    {
+      text: 'Russia',
+      backgroundImage: '',
+      hook: 'scale:name=scale-x,index=1',
+      offsetX: '-50px',
+      width: '50px',
+      height: '50px'
+    },
+    {
+      text: 'Sweden',
+      backgroundImage: '',
+      hook: 'scale:name=scale-x,index=0',
+      offsetX: '-50px',
+      width: '50px',
+      height: '50px'
+    }
+  ],
+  // Values based on "Offshore Wind and Grid in the Baltic Sea – Status and Outlook until 2050"
+  //22 March 2019 Swedish Agency for Marine and Water Management RISE Research Institutes of Sweden Authors: Johannes Hüffmeier, Mats Goldberg (RISE)
+  series: [{
+      text: 'Operational (parks in service)',
+      values: [200, 0, 0, 0, 0, 689, 90, 0, 880],
+      backgroundColor: '#4ff080'
+    },
+    {
+      text: 'Under Construction (ongoing construction work)',
+      values: [0, 0, 0, 0, 0, 385, 0, 0, 598],
+      backgroundColor: '#4fbff0'
+    },
+    {
+      text: 'Approved',
+      values: [2760, 0, 2400, 0, 0, 504, 0, 0, 140],
+      backgroundColor: '#f04fbf'
+    },
+    {
+      text: 'Planned',
+      values: [3080, 433, 8610, 1630, 0, 271, 2070, 433, 640],
+      backgroundColor: '#f0804f'
+    }
+  ]
+};
+
+zingchart.render({
+  id: 'myChart',
+  data: chartConfig
 });
